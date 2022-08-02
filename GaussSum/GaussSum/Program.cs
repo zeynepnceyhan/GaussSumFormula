b811e8239977071b9a4ca478b044a0eec12a7962
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-02 15:51:24 +0300

    gauss sum formula
    internship

---
+++ GaussSum/GaussSum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                sum += i;
+                
+            }
+            Console.WriteLine("The sum (Gauss Sum Formula): " + sum);
+
+            double avg;
+
+            avg = sum / number;
+
+            if (avg > 50)
+            {
+                Console.WriteLine("The average is greater than 50...");
+            }
+            else
+            {
+                Console.WriteLine("The average is less than 50...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
